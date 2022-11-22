e5edc823428eee248a67d86c6c190fb5dcfcd55c
jesf1811 <117354904+jesf1811@users.noreply.github.com>
2022-11-22 19:20:02 +0000

    Update Backtracking.java

--- src/main/java/com/training/accolite/week1/Backtracking.java
+++ src/main/java/com/training/accolite/week1/Backtracking.java
@@ -42,7 +42,57 @@ public class Backtracking {
 	}
  
 	private int go(int step, int errors, boolean[] example) {
-		// Write your code here...
+		System.out.println("step: " + step);
+
+        if (step == example.length){
+            System.out.println("Inside if");
+            if (errors != 0) {
+                System.out.println("Returning 0 - 1st");
+                return 0;
+            }
+
+            System.out.println("Inside for loop");
+            System.out.println("Ones - " + Arrays.toString(ones));
+            System.out.println("s[1] - " + Arrays.toString(s[1]));
+            for (int i = 1; i < s.length; i++){
+                int numberOfErrorsInOnes = 0;
+                for (int j = 0; j < example.length; j++){
+                    if (ones[j] != s[i][j]){
+                        System.out.println("Adding...");
+                        numberOfErrorsInOnes++;
+                    }
+                }
+
+                if (numberOfErrorsInOnes != c[i]){
+                    System.out.println("Returning 0 - 2nd");
+                    return 0;
+                }
+            }
+
+            System.out.println("Returning 1");
+            return 1;
+        }
+
+        ones[step] = example[step];
+        System.out.println("Recurse - 1st part started");
+        int res = go (step+1, errors, example);
+        System.out.println("Recurse - 1st part ended");
+        System.out.println("Before:");
+        System.out.println("Ones - " + Arrays.toString(ones));
+        System.out.println("Example - " + Arrays.toString(example));
+        ones[step] = !example[step];
+        System.out.println("After:");
+        System.out.println("Ones - " + Arrays.toString(ones));
+        System.out.println("Example - " + Arrays.toString(example));
+        System.out.println("Errors - " + errors);
+        if (errors != 0){
+            System.out.println("Recurse - 2nd part started");
+            res += go(step+1, errors-1, example);
+            System.out.println("Recurse - 2nd part ended");
+        }
+
+        System.out.println("Resultant: " + res);
+        return res;
 	}
  
 	public int run() {
@@ -52,6 +102,13 @@ public class Backtracking {
 	}
 	
 	public static void main(String[] args) {
-		new Backtracking().run();
+		//new Backtracking().run();
+		
+		Backtracking backtracking = new Backtracking();
+        backtracking.s = new boolean[][]{{false, false, false, false, false, false,}, {false, true, false, true, false, false}};
+        backtracking.c = new int[]{2,4};
+        backtracking.ones = new boolean[6];
+        System.out.println("Result: " + backtracking.go(0, backtracking.c[0], backtracking.s[0]));
+    
 	}	
 }
